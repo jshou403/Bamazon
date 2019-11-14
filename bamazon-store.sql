f2693b52173c21f393f5b0285d5cbcd7afa65c2a
JACK_E <jshou403@gmail.com>
2019-11-14 17:06:31 -0500

    Changed table values.

--- bamazon-store.sql
+++ bamazon-store.sql
@@ -12,31 +12,31 @@ CREATE TABLE inventory (
 );
 
 INSERT INTO inventory (item, category, price, quantity)
-VALUES ("Pack of Chicken Chews", "Pet - Dog - Treats", 10.00, 40);
+VALUES ("Pack of Chicken Chews", "Dog - Treats", 10.00, 40);
 
 INSERT INTO inventory (item, category, price, quantity)
-VALUES ("Individiual Peanut Butter Bone", "Pet - Dog - Treats", 2.00, 75);
+VALUES ("Individiual Peanut Butter Bone", "Dog - Treats", 2.00, 75);
 
 INSERT INTO inventory (item, category, price, quantity)
-VALUES ("Individual Salmon Jerky Piece", "Pet - Dog - Treats", 2.75, 75);
+VALUES ("Individual Salmon Jerky Piece", "Dog - Treats", 2.75, 75);
 
 INSERT INTO inventory (item, category, price, quantity)
-VALUES ("Stuffed Squirrel", "Pet - Dog - Toys", 7.00, 50);
+VALUES ("Stuffed Squirrel", "Dog - Toys", 7.00, 50);
 
 INSERT INTO inventory (item, category, price, quantity)
-VALUES ("Stuffed Hippo", "Pet - Dog - Toys", 8.00, 50);
+VALUES ("Stuffed Hippo", "Dog - Toys", 8.00, 50);
 
 INSERT INTO inventory (item, category, price, quantity)
-VALUES ("Stuffed Pig", "Pet - Dog - Toys", 9.00, 25);
+VALUES ("Stuffed Pig", "Dog - Toys", 9.00, 25);
 
 INSERT INTO inventory (item, category, price, quantity)
-VALUES ("Hoodie - Red (Xsmall)", "Pet - Dog - Accessories", 10.00, 25);
+VALUES ("Hoodie - Red (Xsmall)", "Dog - Accessories", 10.00, 25);
 
 INSERT INTO inventory (item, category, price, quantity)
-VALUES ("Hoodie - Red (Small)", "Pet - Dog - Accessories", 12.00, 50);
+VALUES ("Hoodie - Red (Small)", "Dog - Accessories", 12.00, 50);
 
 INSERT INTO inventory (item, category, price, quantity)
-VALUES ("Hoodie - Red (Medium)", "Pet - Dog - Accessories", 14.00, 50);
+VALUES ("Hoodie - Red (Medium)", "Dog - Accessories", 14.00, 50);
 
 INSERT INTO inventory (item, category, price, quantity)
-VALUES ("Hoodie - Red (Large)", "Pet - Dog - Accessories", 16.00, 25); 
+VALUES ("Hoodie - Red (Large)", "Dog - Accessories", 16.00, 25); 
