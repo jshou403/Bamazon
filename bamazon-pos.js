418489e3a2951b2c2551f49d12dcb793b289d0de
JACK_E <jshou403@gmail.com>
2019-11-17 17:47:38 -0500

    Last 2 of 4 functions added and working: 3) update inventory and 4) add new item.

--- bamazon-pos.js
+++ bamazon-pos.js
@@ -21,10 +21,6 @@ connection.connect(function (err) {
 
 });
 
-function endDbConnection() {
-    connection.end();
-}
-
 function displayTasks() {
     inquirer.prompt([
         {
@@ -43,24 +39,25 @@ function displayTasks() {
                 viewInventory();
                 break;
             case "View Low Inventory":
-                console.log("\n Low In Stock:");
+                console.log("\n Low Stock:");
                 viewLowInventory();
                 break;
             case "Update Inventory":
-                console.log("\n Update Inventory SELECTED!!!");
+                console.log("\n Add Inventory for... ");
+                updateInventoryPrompt();
                 break;
             case "Add New Item":
-                console.log("\n Add New Item SELECTED!!!");
+                console.log("\n New Item... ");
+                addNewPrompt();
                 break;
             case "Exit":
                 console.log("\n ~~~~~ Exiting Manager Tasks ~~~~~ \n ~~~~~ Goodbye! ~~~~~");
-                endDbConnection();
+                connection.end();
                 break;
             default:
                 console.log("Invalid selection!!!!!");
                 break;
         }
-
     })
 }
 
@@ -74,7 +71,7 @@ function viewInventory() {
 
         for (var i = 0; i < res.length; i++) {
 
-            console.table("Item #" + res[i].id + ": " + res[i].item + " - $" + res[i].price + " (Qty Available: " + res[i].quantity + ") \n");
+            console.log("Item #" + res[i].id + ": " + res[i].category + " - " + res[i].item + " - $" + res[i].price + " (Qty Available: " + res[i].quantity + ") \n");
 
         }
 
@@ -95,7 +92,7 @@ function viewLowInventory() {
 
         for (var i = 0; i < res.length; i++) {
 
-            console.table("Item #" + res[i].id + ": " + res[i].item + " - $" + res[i].price + " (Qty Available: " + res[i].quantity + ") \n");
+            console.table("Item #" + res[i].id + ": " + res[i].category + " - " + res[i].item + " - $" + res[i].price + " (Qty Available: " + res[i].quantity + ") \n");
 
         }
 
@@ -103,6 +100,155 @@ function viewLowInventory() {
 
         displayTasks();
     }
+    )
+
+}
+
+function updateInventoryPrompt() {
+
+    inquirer.prompt([
+        {
+            name: "id",
+            type: "prompt",
+            message: "Item ID#:"
+        },
+        {
+            name: "quantity",
+            type: "prompt",
+            message: "Add:"
+        }
+    ]).then(function (update) {
+
+        // store response for ID into variable
+        var updateID = update.id;
+        // store response for new quantity into variable
+        var addQuantity = update.quantity;
+
+        connection.query(
+            "SELECT * FROM inventory WHERE ?", 
+            {
+                id: updateID
+            },
+            function (err, res) {
+                if (err) throw err; 
+
+                var currentQuantity = res[0].quantity;
+                
+                newQuantity = parseInt(currentQuantity) + parseInt(addQuantity);
+
+                updateInventory(updateID, newQuantity);
+            }
+        )
+
+    })
+
+}
+
+function updateInventory(updateID, newQuantity) {
+
+    connection.query(
+        "UPDATE inventory SET ? WHERE ?",
+        [
+            {
+                quantity: newQuantity
+            },
+            {
+                id: updateID
+            }
+        ],
+        function (err, res) {
+            if (err) throw err;
+
+            console.log("\n" + res.affectedRows + " item updated:");
+
+            // console.log("Item #" + updateID + " - New Quantity = " + newQuantity + "\n");
+
+            displayUpdate(updateID);
+        }
+    )
+
+}
+
+function displayUpdate(updateID) {
+
+    connection.query(
+
+        "SELECT * FROM inventory WHERE ?", 
+        {
+            "id": updateID
+        },
+        function(err, res) {
+            if (err) throw err; 
+            
+            console.log("Item #" + res[0].id + ": " + res[0].item + " - $" + res[0].price + " - New Qty: " + res[0].quantity + " \n");
+
+            displayTasks();
+        }
+
+    )
+
+}
+
+function addNewPrompt() {
+
+    inquirer.prompt([
+        {
+            name: "name",
+            type: "input",
+            message: "Item Name:"
+        },
+        {
+            name: "category",
+            type: "input",
+            message: "Category:"
+        },
+        {
+            name: "price",
+            type: "input",
+            message: "Price: $"
+        },
+        {
+            name: "quantity",
+            type: "input",
+            message: "Quantity:"
+        }
+    ]).then(function(newItem) {
+
+        var itemName = newItem.name;
+        var itemCategory = newItem.category;
+        var itemPrice = newItem.price; 
+        var itemQuantity = newItem.quantity;
+
+        addNewToDb(itemName, itemCategory, itemPrice, itemQuantity);
+
+    })
+
+}
+
+function addNewToDb(itemName, itemCategory, itemPrice, itemQuantity) {
+
+    connection.query(
+
+        "INSERT INTO inventory SET ?",
+        {
+            item: itemName,
+            category: itemCategory,
+            price: itemPrice,
+            quantity: itemQuantity
+        },
+        function (err, res) {
+            if (err) throw err; 
+
+            // console.log(res);
+
+            console.log("\n" + res.affectedRows + " item added:");
+
+            console.log(itemCategory + " - " + itemName + " - $" + itemPrice + " (Qty Available: " + itemQuantity + ") \n");
+
+            displayTasks();
+
+        }
+        
     )
 
 }
